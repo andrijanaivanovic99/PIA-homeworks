849d17f3ca87a00369cbaccb31f34aed343ea087
andrijanaivanovic99 <anaivanovic1601@gmail.com>
2021-01-30 23:59:51 +0100

    funkcionalnost za registraciju

---
+++ hw-4/prijava.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="proba.css">
+      
+    </head>
+
+    <body>
+            <div class="container prijava">
+                <h2> Ulogujte se</h2>
+                <input id="username" class="form-control" type="text" autocomplete="off" placeholder="Korisničko ime ili email adresa">
+                <input id="password" class="form-control" type="text" placeholder="Lozinka">
+                <a id="prijava"  href="#"> Prijavite se</a>
+             </div>
+
+            
+             
+    </body>
+</html>
