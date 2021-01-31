4480ce3e5f84e62c42772526dbbdced1965db5bd
andrijanaivanovic99 <anaivanovic1601@gmail.com>
2021-01-31 19:15:31 +0100

    izmena dizajna za registraciju i prijavu

--- hw-4/hw-4/prijava.php
+++ hw-4/hw-4/prijava.php
@@ -1,24 +1,26 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="proba.css">
-      
-    </head>
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+            <link rel="stylesheet" type="text/css" href="proba.css">
+        
+        </head>
 
-    <body>
-            <div class="container prijava">
-                <h2> Ulogujte se</h2>
-                <form action= "prijavamedjufajl.php" method="post">
-                <input name="usr" id="username" class="form-control" type="text" autocomplete="off" placeholder="Korisničko ime ili email adresa">
-                <input name= "pass" id="password" class="form-control" type="text" placeholder="Lozinka">
-                <button type="submit" name="submit"> Prijavite se </button> 
+        <body>
+       
+                <div class="container prijava">
+                
+                    <h2> Ulogujte se</h2>
+                    <form action= "prijavamedjufajl.php" method="post">
+                    <input name="usr" id="username" class="form-control" type="text" autocomplete="off" placeholder="Korisničko ime ili email adresa">
+                    <input name= "pass" id="password" class="form-control" type="text" placeholder="Lozinka">
+                    <button type="submit" name="submit"> Prijavite se </button> 
                 </form>
-             </div>
+                </div>
 
-            
-             
-    </body>
-</html>
+                
+                
+        </body>
+    </html>
